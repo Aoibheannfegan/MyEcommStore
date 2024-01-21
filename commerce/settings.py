5e078bb383739ee45f99e137d858a2d95de20cc6
Aoibheann <aoibheannfegan04@gmail.com>
2024-01-21 11:51:54 -0800

    Added allowedhosts and CSFR

--- commerce/settings.py
+++ commerce/settings.py
@@ -17,6 +17,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['web-production-fd0dc.up.railway.app', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-fd0dc.up.railway.app']
 
+
 # Application definition
 
 INSTALLED_APPS = [
